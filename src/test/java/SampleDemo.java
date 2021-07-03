182bbbf3eb80c1f4a34930f6b267e0b5809b295b
qavbox <64853324+qavbox@users.noreply.github.com>
2021-07-03 12:29:40 +0530

    Update SampleDemo.java

--- src/test/java/SampleDemo.java
+++ src/test/java/SampleDemo.java
@@ -3,5 +3,6 @@ public class SampleDemo {
     public static void main(String []args){
         System.out.println("Welcome");
         System.out.println("JTC INDIA");
+        System.out.println("Keep learning");
     }
 }
